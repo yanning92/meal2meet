63488cbace7ca3cb954da269e21cad67e7ac1cc6
yanning92 <yanning86@gmail.com>
2017-09-23 12:09:53 +0200

    init firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,16 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {AngularFireModule} from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC0G-jwbLWo8PmxlZNGqEeLvCj97ts9Aw8",
+  authDomain: "mealtomeat.firebaseapp.com",
+  databaseURL: "https://mealtomeat.firebaseio.com",
+  projectId: "mealtomeat",
+  storageBucket: "",
+  messagingSenderId: "1072662515798"
+};;
 
 @NgModule({
   declarations: [
@@ -14,7 +24,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
